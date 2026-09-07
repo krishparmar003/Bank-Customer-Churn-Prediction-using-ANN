afee7ebb76b236355a3e3b0cc1cc0acad246b15f
Krish Parmar <krishparmar003@gmail.com>
2026-09-07 18:40:28 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = tf.keras.models.load_model('model.h5')
+
+# Load the encoders and scaler
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
+## streamlit app
+st.title('Customer Churn PRediction')
